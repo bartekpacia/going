07b603909f81febcaa7b284829d99e069c9df100
Bartek Pacia <barpac02@gmail.com>
2020-10-24 22:33:02 +0200

    do some concurrency

---
+++ lang_features/concurrency/goroutines/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	publish("Jebać pis!", 5*time.Second)
+	fmt.Println("Ciekawe co tam słychać w Polsce...")
+
+	time.Sleep(10 * time.Second)
+	println("Prawidłowo!")
+}
+
+func publish(news string, delay time.Duration) {
+	go func() {
+		time.Sleep(delay)
+		fmt.Println(news)
+	}()
+}
