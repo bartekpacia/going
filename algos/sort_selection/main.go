a1abee7a41d46fd7f30c5e07190ca1569f00f08c
Bartek Pacia <barpac02@gmail.com>
2021-01-20 08:53:08 +0100

    add insertion sort

---
+++ algos/sort_selection/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func selectionSort(arr []int) []int {
+	n := len(arr)
+	for i := 1; i < n; i++ {
+		for j := 0; j < i; j++ {
+			if arr[j] > arr[i] {
+				arr[j], arr[i] = arr[i], arr[j]
+			}
+		}
+	}
+
+	return arr
+}
+
+func main() {
+	nums := []int{7, 9, 8, 3, 2, 1, 6, 4, 5}
+
+	selectionSort(nums)
+
+	fmt.Println(nums)
+}
